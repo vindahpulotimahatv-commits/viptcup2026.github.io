7e5dc60285c31f105080cdc7fe99cfba652c4ec6
vindahpulotimahatv-commits <vindahpulotimahatv@gmail.com>
2026-08-20 12:11:12 +0700

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.viptcup.iv2026',
   appName: 'VIPT CUP IV 2026',
-  webDir: '.',
+  webDir: 'www',
   bundledWebRuntime: false
 };
 
